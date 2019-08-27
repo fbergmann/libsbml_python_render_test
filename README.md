dbc0a72ed1683a1ce37c5a10a827c4e62ea491bb
Frank T. Bergmann <fbergman@caltech.edu>
2019-08-27 09:47:37 +0200

    - initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+## LibSBML Render Test project
+This project tests the libsbml render package, to ensure all methods are there, and things are working as they should be. 
+
+To run the unit tests contained here, create a new virtual environment with the python version of your choice and run `python test_render.py`
+
+### Conda
+To create the virtual environment with conda use: 
+
+
+	conda create -n libsbml_37 python=3.7
+	conda activate libsbml_37
+	pip install python-libsbml-experimental
+	python test_render.py
+
+the expected output would be: 
+
+
+	Using libSBML: 5.18.1
+	Using Python: 3.7.4 (default, Aug  9 2019, 18:34:13) [MSC v.1915 64 bit (AMD64)]
+	.......
+	----------------------------------------------------------------------
+	Ran 7 tests in 0.045s
+
+	OK
+
+### venv
+Without conda, you'd create an environment like so (on windows): 
+
+	python -m venv libsbml_36
+	.\libsbml_36\Scripts\activate.bat
+	pip install python-libsbml-experimental
+	python test_render.py
+
+
+## License 
+This project is open source and freely available under the [Simplified BSD](http://opensource.org/licenses/BSD-2-Clause) license. Should that license not meet your needs, please contact me. 
+
+
+Copyright (c) 2019, Frank T. Bergmann  
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
---
+++ test_render.py
@@ -0,0 +1,100 @@
+import unittest
+import libsbml
+import sys
+
+
+STYLE_SHEET = 'SBGNstyles_31.xml'
+
+print('Using libSBML: %s' % libsbml.getLibSBMLDottedVersion())
+print('Using Python: %s' % sys.version)
+
+
+class TestRender(unittest.TestCase):
+
+    def test_version(self):
+        assert libsbml.getLibSBMLDottedVersion() == '5.18.1'
+
+    def test_libsbml_has_render(self):
+        ns = libsbml.RenderPkgNamespaces(3, 1)
+        assert ns is not None
+
+    def test_libsbml_has_layout(self):
+        ns = libsbml.LayoutPkgNamespaces(3, 1)
+        assert ns is not None
+
+    def test_sbase_has_read(self):
+        compartment = libsbml.Compartment(3, 1)
+        assert compartment is not None
+        stream = libsbml.XMLInputStream('<compartment id="vol1" name="compartment 1" size="2.0" />', False)
+        compartment.read(stream)
+        assert compartment.id == 'vol1'
+        assert compartment.name == 'compartment 1'
+        assert compartment.volume == 2.0
+
+    def test_downcast_polygon(self):
+        group = libsbml.RenderGroup(3, 1)
+        stream = libsbml.XMLInputStream("""
+      <g stroke="black" stroke-width="0.1" fill="black">
+        <polygon>
+          <listOfElements>
+            <element xsi:type="RenderPoint"  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" x="0.0" y="0.0"/>
+            <element xsi:type="RenderPoint"  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" x="0.3" y="0.0"/>
+            <element xsi:type="RenderPoint" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"  x="0.3" y="8.0"/>
+            <element xsi:type="RenderPoint"  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" x="0.0" y="8.0"/>
+          </listOfElements>
+        </polygon>
+      </g>        """, False)
+        group.read(stream)
+        assert group.stroke == 'black'
+        assert group.fill == 'black'
+        assert group.stroke_width == 0.1
+        assert group.getNumElements() > 0
+
+        p = group.getElement(0)
+        assert isinstance(p, libsbml.Polygon)
+        for elt in p.getListOfElements():
+            assert(isinstance(elt, libsbml.RenderPoint))    
+
+    def test_read_stylesheet_31(self):
+        info = libsbml.LocalRenderInformation(3, 1)
+        stream = libsbml.XMLInputStream(STYLE_SHEET, True)
+        info.read(stream)
+        assert info.id == 'SBGN_default'
+        line = info.getLineEnding('inhibitor')
+        assert isinstance(line, libsbml.LineEnding)
+        assert line.id == 'inhibitor'
+        group = line.getGroup()
+        assert isinstance(group, libsbml.RenderGroup)
+        assert group.stroke == 'black'
+        assert group.fill == 'black'
+        assert group.stroke_width == 0.1
+        assert group.getNumElements() > 0
+
+        p = group.getElement(0)
+        assert isinstance(p, libsbml.Polygon)
+        for elt in p.getListOfElements():
+            assert(isinstance(elt, libsbml.RenderPoint))    
+
+    def test_read_stylesheet_24(self):
+        info = libsbml.LocalRenderInformation(2, 4)
+        stream = libsbml.XMLInputStream(STYLE_SHEET, True)
+        info.read(stream)
+        assert info.id == 'SBGN_default'
+        line = info.getLineEnding('inhibitor')
+        assert isinstance(line, libsbml.LineEnding)
+        assert line.id == 'inhibitor'
+        group = line.getGroup()
+        assert isinstance(group, libsbml.RenderGroup)
+        assert group.stroke == 'black'
+        assert group.fill == 'black'
+        assert group.stroke_width == 0.1
+        assert group.getNumElements() > 0
+
+        p = group.getElement(0)
+        assert isinstance(p, libsbml.Polygon)
+        for elt in p.getListOfElements():
+            assert(isinstance(elt, libsbml.RenderPoint))    
+
+
+if __name__ == '__main__':
+    unittest.main()
